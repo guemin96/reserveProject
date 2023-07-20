4cfb168e0b2fb14a062c6f8ceb0b6f192ca2bd5a
ParkGyumin <guemin1212@naver.com>
2023-07-20 21:34:38 +0900

    1일차 작업 완료

---
+++ ReserveBack/src/main/java/com/example/reserveback/ReserveBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.reserveback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveBackApplication.class, args);
+	}
+
+}
---
+++ ReserveBack/src/test/java/com/example/reserveback/ReserveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reserveback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
